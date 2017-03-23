66b2a56634f9e33055b747a1a7ab1891c3451ee9
MatthewBeaudouinLafon <matthew.beaudouin-lafon@students.olin.edu>
2017-03-23 00:38:51 -0400

    Added project tiling

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -3,6 +3,29 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
+        return (
+            // <div className="app">Hello, World!</div>
+            <ProjectGrid projectList={this.props.projectList} />
+        );
+    }
+}
+
+class ProjectGrid extends React.Component {
+    render() {
+        var projects = [];
+        this.props.projectList.forEach(function(project) {
+            projects.push(<ProjectItem className="project-item" project={project} key={project.id}/>)
+        })
+        return (
+            <div className="project-grid">
+                {projects}
+            </div>
+        );
+    }
+}
+
+class ProjectItem extends React.Component {
+    render() {
         var name = this.props.project.name;
         var authorList = this.props.project.students;
         var description = this.props.project.description;
