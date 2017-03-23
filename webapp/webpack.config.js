aba278938c6e050ee450f2719db0f59bf3a9fd03
YehEmily <emilyyeh1997@gmail.com>
2017-03-23 00:39:19 -0400

    Updated CSS

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -6,7 +6,7 @@ var APP_DIR = path.resolve(__dirname, './src');
 
 var config = {
     entry: [
-        'webpack-dev-server/client?http://localhost:8080',
+        'webpack-dev-server/client?http://localhost:9090',
         'webpack/hot/only-dev-server',
         APP_DIR + '/index.js'
     ],
