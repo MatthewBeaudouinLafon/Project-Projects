b253a3178b81e2f7ea0c449434ad558ec0e0297a
MatthewBeaudouinLafon <matthew.beaudouin-lafon@students.olin.edu>
2017-03-23 01:21:32 -0400

    Added scroll functionality and more examples

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -18,7 +18,22 @@ var PROJECTS = [
     id: "2", 
     students: ["Alan Turing", "Bletchley Park"], 
     thumbnail: "http://static.bbc.co.uk/history/img/ic/640/images/resources/topics/enigma.jpg", 
-    description: "Math to win WWII."
+    description: "Math to win WWII.  Turing played a pivotal role in cracking intercepted coded messages that enabled the Allies to defeat the Nazis in many crucial engagements, including the Battle of the Atlantic, and in so doing helped win the war."
+  },
+  {
+    name: "National Tour",
+    id: "3",
+    students: ["Itzhak Perlman", "Emanuel Ax"],
+    thumbnail: "https://www.noozhawk.com/images/uploads/nh_7_perlman_ax.jpg  (673KB) ",
+    description: "Two of the world's most accomplished classical musicians and longtime friends Itzhak Perlman and Emanuel Ax did a national tour in 2016."
+  },
+
+  {
+    name: "Puppy Celebration",
+    id: "4",
+    students: ["Barack Obama", "Michelle Obama", "Malia Obama", "Sasha Obama"],
+    thumbnail: "",
+    description: "Even though the Obamas are out of the White House, we can't help reminiscing about our favorite former first pooches, Bo and Sunny. Michelle Obama recently gave the world a glimpse of what Bo and Sunny are up to since leaving the White House -- and not surprisingly, they're as happy as ever."
   }
 ];
 
