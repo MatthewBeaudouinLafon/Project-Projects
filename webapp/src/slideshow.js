e24728c4e5cb507a1f4d094f67152946b931235e
YehEmily <emilyyeh1997@gmail.com>
2017-04-06 14:36:48 -0400

    Added next and prev buttons

--- webapp/src/slideshow.js
+++ webapp/src/slideshow.js
@@ -1,22 +1,46 @@
-var slideIndex = 0;
-showSlides();
+var slideIndex = 30;
+showSlides(slideIndex);
 
-function showSlides() {
+function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    // var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+
     for (i = 0; i < slides.length; i++) {
        if (slides[i] != null) {
-        console.log("NOT NULL");
         slides[i].visibility = "hidden";
         slides[i].style.display = "none";
        }
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1;}    
+
     if (slides[slideIndex-1] != null) {
-        console.log("VISIBLE");
         slides[slideIndex-1].visibility = "visible";
         slides[slideIndex-1].style.display = "block";
     }
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+
+    // var i;
+    // var slides = document.getElementsByClassName("mySlides");
+    // var counter = document.getElementsByClassName("slide-counter");
+    // var total = slides.length;
+    // var current = 1;
+
+    // if (n > slides.length) {slideIndex = 1}    
+    // if (n < 1) {slideIndex = slides.length}
+
+    
+    // slideIndex++;
+    // if (slideIndex > slides.length) {slideIndex = 1;}    
+    
+    // // setTimeout(showSlides, 2000); // Change image every 2 seconds
+
+export const plusSlides = (n)=>{
+    showSlides(slideIndex += n);
+}
+
+export const minusSlides = (n) => {
+    showSlides(slideIndex -= n);
 }
